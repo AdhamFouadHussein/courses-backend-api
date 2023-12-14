4ab66f61c62883f7c9a002b436ade43e58c3585c
Adham Fouad <adhamfouad49@gmail.com>
2023-12-14 22:55:18 +0200

    Register and login is now working

--- login.php
+++ login.php
@@ -3,70 +3,50 @@ header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
-// Database configuration
-	$servername = "localhost";
-	$username = "doma";
-	$passwordd = "password";
-	$dbname = "tafl";
+
+$servername="localhost";
+$username="doma";
+$passwordd="password";
+$dbname="tafl";
 
 try {
-    // Create connection
-    $conn = new mysqli($servername, $username, $passwordd, $dbname);
-    // Set the charset to UTF-8
+    $conn=new mysqli($servername,$username,$passwordd,$dbname);
     $conn->set_charset("utf8");
-    // Check connection
-    if ($conn->connect_error) {
-        throw new Exception("Connection failed: " . $conn->connect_error);
+    if($conn->connect_error){
+        throw new Exception("Connection failed: ".$conn->connect_error);
     }
 
-    // Get JSON as a string
-    $json_str = file_get_contents('php://input');
+    $json_str=file_get_contents('php://input');
+    $json_obj=json_decode($json_str, true);
 
-    // Get as an object
-    $json_obj = json_decode($json_str);
+    $email=mysqli_real_escape_string($conn, $json_obj['email']);
+    $password=$json_obj['password'];
 
-    $email = $json_obj->email;
-    $password = $json_obj->password;
-
-    // Prepare and bind
-    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
-    $stmt->bind_param("s", $email);
-
-    // Execute the statement
+    $stmt=$conn->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->bind_param("s",$email);
     $stmt->execute();
+    $result=$stmt->get_result();
 
-    // Get the result
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        // User exists, now we verify the password
-        // Fetch the user data
-        $user = $result->fetch_assoc();
-
-        // Verify the password
-        if (password_verify($password, $user['password'])) {
-            // Correct password
-            $token = bin2hex(random_bytes(16)); // This generates a crypto-secure 32 characters long token
-            $stmt = $conn->prepare("UPDATE `users` SET `token`= ? WHERE `email` = ?");
-            $stmt->bind_param("ss",$token, $email ); 
-            // Execute the statement
+    if($result->num_rows>0){
+        $user=$result->fetch_assoc();
+        if(password_verify($password, $user['password'])){
+            $token=bin2hex(random_bytes(16));
+            $stmt=$conn->prepare("UPDATE `users` SET `token`= ? WHERE `email` = ?");
+            $stmt->bind_param("ss",$token,$email);
             $stmt->execute();
             $stmt->close();
             $conn->close();
-            $response = array('status' => 200, 'message' => 'Logged in', 'token' => $token);
-            $response = array_merge($response, $user); // Merge the arrays
-        } else {
-            // Incorrect password
-            $response = array('status' => 401, 'message' => 'Incorrect password');
+            $response=array('status'=>200,'message'=>'Logged in','token'=>$token);
+            $response=array_merge($response,$user);
+        }else{
+            $response=array('status'=>401,'message'=>'Incorrect password');
         }
-    } else {
-        // User does not exist
-        $response = array('status' => 404, 'message' => 'User does not exist');
+    }else{
+        $response=array('status'=>404,'message'=>'User does not exist');
     }
-
     echo json_encode($response);
-} catch (Exception $e) {
+}catch(Exception $e){
     http_response_code(500);
-    echo json_encode(array('error' => $e->getMessage()));
+    echo json_encode(array('error'=>$e->getMessage()));
 }
 ?>
---
+++ register.php
@@ -0,0 +1,55 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+$servername = "localhost";
+$username = "doma";
+$password = "password";
+$dbname = "tafl";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    throw new Exception("Connection failed: " . $conn->connect_error);
+}
+
+$json_str = file_get_contents('php://input');
+$json_obj = json_decode($json_str, true);
+
+$username = mysqli_real_escape_string($conn, $json_obj['username']);
+$email = mysqli_real_escape_string($conn, $json_obj['email']);
+$passwordd = password_hash($json_obj['password'], PASSWORD_DEFAULT);
+
+$stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND email = ?");
+$stmt->bind_param("ss", $username, $email);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    $response = array('status' => 401, 'message' => 'Username or Email already exists');
+} else {
+    $token = bin2hex(random_bytes(16));
+    $stmt = $conn->prepare("INSERT INTO users (full_name, email, password, username, phone_number, city, address, token) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssss", $json_obj['fullName'], $email, $passwordd, $username, $json_obj['phone_number'], $json_obj['city'], $json_obj['address'], $token);
+    if ($stmt->execute()) {
+        $response = array(
+            'status' => 200, 
+            'message' => 'New record created successfully',
+            'username' => $username,
+            'full_name' => $json_obj['fullName'],
+            'email' => $email,
+            'phone_number' => $json_obj['phone_number'],
+            'city' => $json_obj['city'],
+            'address' => $json_obj['address'],
+            'token' => $token
+        );
+    } else {
+        $response = array('status' => 401, 'message' => 'Error: ' . $stmt->error);
+    }
+}
+
+$conn->close();
+
+header('Content-type: application/json');
+echo json_encode($response);
+?>
