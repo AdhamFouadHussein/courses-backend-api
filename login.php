fbcc73b07c21b4aa313fd4fef4c33fedf5e3cd9b
Adham Fouad <adhamfouad49@gmail.com>
2023-12-14 15:31:07 +0200

    fixed login.php

--- login.php
+++ login.php
@@ -1,19 +1,22 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-    // Database configuration
-    $servername = "localhost";
-    $username = "doma";
-    $passwordd = "password";
-    $dbname = "tafl";
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
+// Database configuration
+	$servername = "localhost";
+	$username = "doma";
+	$passwordd = "password";
+	$dbname = "tafl";
 
+try {
     // Create connection
     $conn = new mysqli($servername, $username, $passwordd, $dbname);
     // Set the charset to UTF-8
     $conn->set_charset("utf8");
     // Check connection
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        throw new Exception("Connection failed: " . $conn->connect_error);
     }
 
     // Get JSON as a string
@@ -37,34 +40,13 @@ header("Access-Control-Allow-Headers: *");
 
     if ($result->num_rows > 0) {
         // User exists, now we verify the password
-        // Prepare and bind
-        $stmt = $conn->prepare("SELECT
-        full_name,
-        email,
-        username,
-        phone_number,
-        city,
-        address
-         FROM users WHERE email = ? AND password = ?");
-        $stmt->bind_param("ss", $email, $password); 
+        // Fetch the user data
+        $user = $result->fetch_assoc();
 
-        // Execute the statement
-        $stmt->execute();
-
-        // Get the result
-        $result = $stmt->get_result();
-        $stmt->close();
-        $conn->close();
-        
-        if ($result->num_rows > 0) {
-             // Correct password
-              // Fetch all rows
-       $rows = array();
-       while($row = $result->fetch_assoc()) {
-           $rows[] = $row;
-       }
+        // Verify the password
+        if (password_verify($password, $user['password'])) {
+            // Correct password
             $token = bin2hex(random_bytes(16)); // This generates a crypto-secure 32 characters long token
-            $conn = new mysqli($servername, $username, $passwordd, $dbname);
             $stmt = $conn->prepare("UPDATE `users` SET `token`= ? WHERE `email` = ?");
             $stmt->bind_param("ss",$token, $email ); 
             // Execute the statement
@@ -72,7 +54,7 @@ header("Access-Control-Allow-Headers: *");
             $stmt->close();
             $conn->close();
             $response = array('status' => 200, 'message' => 'Logged in', 'token' => $token);
-            $response = array_merge($response, $rows[0]); // Merge the arrays
+            $response = array_merge($response, $user); // Merge the arrays
         } else {
             // Incorrect password
             $response = array('status' => 401, 'message' => 'Incorrect password');
@@ -82,8 +64,9 @@ header("Access-Control-Allow-Headers: *");
         $response = array('status' => 404, 'message' => 'User does not exist');
     }
 
-   
-
-    header('Content-type: application/json');
     echo json_encode($response);
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(array('error' => $e->getMessage()));
+}
 ?>
